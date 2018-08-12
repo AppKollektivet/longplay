7691c8d578c575955792d9c7616b6536a844be2e
Elvis Nuñez <elvisnunez@me.com>
2018-08-12 17:06:39 +0200

    Add base controller

--- Longplay/ContainerViewController.swift
+++ Longplay/ContainerViewController.swift
@@ -3,6 +3,12 @@ import UIKit
 class ContainerViewController: UIViewController {
     init() {
         super.init(nibName: nil, bundle: nil)
+
+        view.backgroundColor = .black
+    }
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 
     required init?(coder aDecoder: NSCoder) {
