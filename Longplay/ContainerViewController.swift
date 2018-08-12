f029ad2afa69492123b1ad990ace16d4508575af
Elvis Nuñez <elvisnunez@me.com>
2018-08-12 17:02:55 +0200

    Initial commit

---
+++ Longplay/ContainerViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class ContainerViewController: UIViewController {
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()        
+    }
+}
